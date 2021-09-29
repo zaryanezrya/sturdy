48d8a8a4d3477f897e3053184f1316e8c4a935a5
Ivan Sharun <ivan@sha.run>
2021-09-29 16:21:29 +0000

    Added PluginManager

---
+++ tests/mocks/plugins/math_helper/__init__.py
@@ -0,0 +1,16 @@
+from sturdy.ioc import resolve
+from sturdy.ioc.interfaces import ICommand
+from sturdy.plugins.interfaces import IPlugin
+
+
+class Plugin(IPlugin):
+    def get_load_command(self) -> ICommand:
+        return PluginLoadCommand()
+
+    def get_unload_command(self) -> ICommand:
+        ...
+
+
+class PluginLoadCommand(ICommand):
+    def __call__(self) -> None:
+        resolve("IoC.Register", "MathHelper.add", lambda *args: args[0] + args[1])()
