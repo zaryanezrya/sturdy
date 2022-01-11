25376cb97854eaacda616619ead8037d6681ad7f
Ivan Sharun <ivan@sha.run>
2022-01-11 16:25:30 +0000

    Plugin interface has been deleted

--- tests/mocks/plugins/math_helper/__init__.py
+++ tests/mocks/plugins/math_helper/__init__.py
@@ -1,14 +1,5 @@
 from sturdy.ioc import resolve
 from sturdy.interfaces import ICommand
-from sturdy.plugins.interfaces import IPlugin
-
-
-class Plugin(IPlugin):
-    def get_load_command(self) -> ICommand:
-        return PluginLoadCommand()
-
-    def get_unload_command(self) -> ICommand:
-        ...
 
 
 class PluginLoadCommand(ICommand):
---
+++ tests/sturdy/plugins/test_plugin_load.py
@@ -0,0 +1,12 @@
+import unittest
+
+from sturdy.ioc import resolve
+
+
+class TestPluginLoadCommand(unittest.TestCase):
+    def test_plugin_load_success(self):
+        from tests.mocks.plugins.math_helper import PluginLoadCommand
+
+        PluginLoadCommand()()
+
+        self.assertEqual(3, resolve("MathHelper.add", 1, 2))
