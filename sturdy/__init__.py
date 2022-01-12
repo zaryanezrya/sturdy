f12df6953a314345cdf069cb8b3088dcad6e4520
Ivan Sharun <ivan@sha.run>
2022-01-12 16:42:03 +0000

    Added IStrategy import

--- sturdy/__init__.py
+++ sturdy/__init__.py
@@ -2,5 +2,6 @@ from sturdy.core.ioc import resolve
 from sturdy.core.ioc.exceptions import ResolveDependencyException
 
 from sturdy.core.command import ICommand, CommandException
+from sturdy.core.strategy import IStrategy
 
 __version__ = "0.1.2"
