d7db62d70ee59a8b83d79104420a1b04d4e54e27
Ivan Sharun <ivan@sha.run>
2022-03-04 18:05:33 +0000

    Resolve interface changed

--- sturdy/core/ioc/base/ioc_resolve.py
+++ sturdy/core/ioc/base/ioc_resolve.py
@@ -8,6 +8,6 @@ class IOCBaseResolveResolver(IStrategy):
     def __init__(self, container: IDependenciesContainer):
         self.__container = container
 
-    def __call__(self, *args: Any) -> IStrategy:
+    def __call__(self, *args: Any, **kwargs: Any) -> IStrategy:
         key = args[0]
-        return self.__container[key](*args[1:])
+        return self.__container[key](*args[1:], **kwargs)
