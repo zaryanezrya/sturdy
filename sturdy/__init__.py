20619ebf6bc161273e1d455e68544e735702856b
Ivan Sharun <ivan@sha.run>
2023-03-14 20:52:03 +0600

    Moving to poetry

--- sturdy/__init__.py
+++ sturdy/__init__.py
@@ -4,5 +4,3 @@ from sturdy.core.ioc.exceptions import ResolveDependencyException
 from sturdy.core.command import ICommand, CommandException
 from sturdy.core.strategy import IStrategy
 from sturdy.core.iuobject import IUObject
-
-__version__ = "0.1.4"
